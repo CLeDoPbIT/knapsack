84f4b7e4b5c68ea86e52f569d8a5ca373400f2cf
Burashnikov <Evgeny.Burashnikov@harman.com>
2020-03-12 14:56:58 +0300

    Fixing bug in common dp approach

--- dynamic_programming.py
+++ dynamic_programming.py
@@ -1,14 +1,30 @@
 import numpy as np
+import pandas as pd
 
 
-def solve_knapsack_problem(value, weights_subjects, weight_knapsack):
-    matrix = np.zeros((len(weights_subjects), weight_knapsack+1))
-    for i in range(1, len(weights_subjects)):
-        for w in range(1, weight_knapsack+1):
-            if w-weights_subjects[i] >= 0:
-                matrix[i][w] = max(value[i] + matrix[i-1][w-weights_subjects[i]], matrix[i-1][w])
-    print(matrix)
+def solve_knapsack_problem_dp(data, weight_knapsack):
+
+    weights_subjects = list(data["weight"])
+    value = list(data["value"])
+
+    matrix = np.zeros((len(weights_subjects)+1, weight_knapsack+1))
+    for i in range(len(weights_subjects)+1):
+        for w in range(weight_knapsack+1):
+            if i == 0 or w == 0:
+                matrix[i][w] = 0
+            elif w-weights_subjects[i-1] >= 0:
+                matrix[i][w] = max(value[i-1] + matrix[i-1][w-weights_subjects[i-1]], matrix[i-1][w])
+            else:
+                matrix[i][w] = matrix[i-1][w]
+    for i in matrix:
+        print(i)
+
     return matrix[-1][-1]
+
+
+def solve_knapsack_problem_dp_evolved(data, weight_knapsack):
+    data = data.sort_values(by=["weight"])
+    # todo: need make tree for fast calculating needed cells from dp table
 
 
 def read_data_from_file(filename):
@@ -21,14 +37,16 @@ def read_data_from_file(filename):
         if id>0:
             weight_items.append(int(data[id].split(" ")[1]))
             value_items.append(int(data[id].split(" ")[0]))
-    return weight_knapsack, weight_items, value_items
+
+    data = pd.DataFrame()
+    data["weight"] = weight_items
+    data["value"] = value_items
+
+    return weight_knapsack, data
 
 
 if __name__ == "__main__":
-    # VALUE = [0, 8, 2, 4, 5]
-    # WEIGHT_SUBJECTS = [0, 5, 1, 3, 2]
-    # WEIGHT_KNAPSACK = 8
-
     filename = 'data/low-dimensional/f3_l-d_kp_4_20'
-    weight_knapsack, weight_items, value_items = read_data_from_file(filename)
-    print(solve_knapsack_problem(value_items, weight_items, weight_knapsack))
+    weight_knapsack, data = read_data_from_file(filename)
+    print(solve_knapsack_problem_dp(data, weight_knapsack))
+
